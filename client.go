ad3ab183b53d00d4ec7a5a71dba66d72100bbad0
Sebastian Sdorra <s.sdorra@gmail.com>
2018-03-22 15:35:42 +0100

    added a function to find out, if a request was the first successful authenticated request in a session

--- client.go
+++ client.go
@@ -383,6 +383,7 @@ func (c *Client) getSession(w http.ResponseWriter, r *http.Request) {
 				glog.Infof("Validated ticket %s for %s", ticket, t.User)
 			}
 
+			setFirstAuthenticatedRequest(r, true)
 			setAuthenticationResponse(r, t)
 			return
 		} else {
