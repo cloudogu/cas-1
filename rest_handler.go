d84e142264c184286ccf0e6709ebd274f7e96efb
Robert Auer <robert.auer@cloudogu.com>
2018-05-04 08:28:03 +0200

    Add cache for requests

--- rest_handler.go
+++ rest_handler.go
@@ -4,56 +4,72 @@ import (
 	"net/http"
 
 	"github.com/golang/glog"
+	"github.com/patrickmn/go-cache"
+	"time"
 )
 
 // restClientHandler handles CAS REST Protocol over HTTP Basic Authentication
 type restClientHandler struct {
-	c *RestClient
-	h http.Handler
+	restClient *RestClient
+	handler    http.Handler
 }
 
 // ServeHTTP handles HTTP requests, processes HTTP Basic Authentication over CAS Rest api
 // and passes requests up to its child http.Handler.
-func (ch *restClientHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (restClientHandler *restClientHandler) ServeHTTP(responseWriter http.ResponseWriter, request *http.Request) {
 	if glog.V(2) {
-		glog.Infof("cas: handling %v request for %v", r.Method, r.URL)
+		glog.Infof("cas: handling %v request for %v", request.Method, request.URL)
 	}
 
-	username, password, ok := r.BasicAuth()
+	username, password, ok := request.BasicAuth()
 	if !ok {
-		w.Header().Set("WWW-Authenticate", "Basic realm=\"CAS Protected Area\"")
-		w.WriteHeader(401)
+		responseWriter.Header().Set("WWW-Authenticate", "Basic realm=\"CAS Protected Area\"")
+		responseWriter.WriteHeader(401)
 		return
 	}
 
 	// TODO we should implement a short cache to avoid hitting cas server on every request
 	// the cache could use the authorization header as key and the authenticationResponse as value
-
-	success, err := ch.authenticate(username, password)
-	if err != nil {
-		if glog.V(1) {
-			glog.Infof("cas: rest authentication failed %v", err)
+	glog.Infof("Setting up request cache")
+	requestCache := cache.New(2*time.Minute, 5*time.Minute)
+	glog.Infof("Checking if authenticationResponse is already in cache")
+	authenticationResponse, keyWasFound := requestCache.Get(username)
+	if !keyWasFound {
+		glog.Infof("authenticationResponse was not already in cache; creating new one...")
+		newAuthenticationResponse, err := restClientHandler.authenticate(username, password)
+		if err != nil {
+			if glog.V(1) {
+				glog.Infof("cas: rest authentication failed %v", err)
+			}
+			responseWriter.Header().Set("WWW-Authenticate", "Basic realm=\"CAS Protected Area\"")
+			responseWriter.WriteHeader(401)
+			return
 		}
-		w.Header().Set("WWW-Authenticate", "Basic realm=\"CAS Protected Area\"")
-		w.WriteHeader(401)
-		return
+		glog.Infof("Adding new authenticationResponse to cache")
+		requestCache.Set(username, newAuthenticationResponse, cache.DefaultExpiration)
+		//TODO: Set firstAuthenticatedRequest
+		glog.Infof("Setting firstAuthenticatedRequest")
+		setFirstAuthenticatedRequest(request, true)
 	}
-
-	setAuthenticationResponse(r, success)
-	ch.h.ServeHTTP(w, r)
+	glog.Infof("Getting authenticationResponse from cache")
+	authenticationResponse, keyWasFound = requestCache.Get(username)
+	glog.Infof("Setting authenticationResponse to request")
+	setAuthenticationResponse(request, authenticationResponse.(*AuthenticationResponse))
+	glog.Infof("Serve request")
+	restClientHandler.handler.ServeHTTP(responseWriter, request)
 	return
 }
 
 func (ch *restClientHandler) authenticate(username string, password string) (*AuthenticationResponse, error) {
-	tgt, err := ch.c.RequestGrantingTicket(username, password)
+	tgt, err := ch.restClient.RequestGrantingTicket(username, password)
 	if err != nil {
 		return nil, err
 	}
 
-	st, err := ch.c.RequestServiceTicket(tgt)
+	st, err := ch.restClient.RequestServiceTicket(tgt)
 	if err != nil {
 		return nil, err
 	}
 
-	return ch.c.ValidateServiceTicket(st)
+	return ch.restClient.ValidateServiceTicket(st)
 }
